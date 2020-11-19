81389695d8f047f025cd8232221591fd557a4d03
Jennifer Joseph <josephjz@bc.edu>
2020-11-18 20:22:49 -0500

    Initial Commit

---
+++ Kid-Friendly Hangman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kid-Friendly Hangman
+//
+//  Created by Jennifer Joseph on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
